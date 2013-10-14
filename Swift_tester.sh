500f048282fd0e9de310d8f1dd6969bb184882ce
Greg Nokes <tsykoduk@gmail.com>
2013-10-14 14:18:32 -0700

    added some output to help debug

--- Swift_tester.sh
+++ Swift_tester.sh
@@ -20,5 +20,8 @@ export HP_SECRET_KEY="CHANGE ME"
 
 export test_file_name=$(basename $test_file)
 export HPAuth=$(curl -s -X POST -H "Content-Type: application/json" https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/tokens -d '{"auth:{"apiAccessKeyCredentials":{"accessKey":"'$HP_ACCESS_KEY'", "secretKey":"'$HP_SECRET_KEY'"}, "tenantId":"'$HP_PROJECT_ID'"}}' | grep "HPAuth" | sed 's/^    \"id\": \"//' | sed 's/\",//') 
+
+echo "Trying to access US-West"
 curl -i -H "x-auth-token: $HPAuth" https://region-a.geo-1.objects.hpcloudsvc.com/v1/$HP_PROJECT_ID/$container/$test_file_name -X PUT -T $test_file
+echo "Trying to access US-East"
 curl -i -H "x-auth-token: $HPAuth" https://region-b.geo-1.objects.hpcloudsvc.com/v1/$HP_PROJECT_ID/$container/$test_file_name -X PUT -T $test_file
