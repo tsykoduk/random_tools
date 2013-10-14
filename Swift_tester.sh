ca0bd35fe24cfdf2fa6c8bb571aaf0512c8e6b54
Greg Nokes <tsykoduk@gmail.com>
2013-10-14 08:40:28 -0700

    added some docs to the swift tester

--- Swift_tester.sh
+++ Swift_tester.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # Collin's Dirty Sed Auth and an example swift call
-#Slightly modified by @tsykoduk
+# Slightly modified by @tsykoduk
 
-#For HP's US-West
+# You'll need to set up a container in USWest and USEast
+# 
+# Change the config below to suit your enviroment
 
-
-export test_file=/home/ubuntu/SOME_FILE
-export test_file_name=$(basename $test_file)
+export test_file=/CHANGE/ME/TO/SOME_FILE
 export container="CHANGE ME"
 export HP_PROJECT_ID="CHANGE ME"
 export HP_ACCESS_KEY="CHANGE ME"
 export HP_SECRET_KEY="CHANGE ME"
-export HPAuth=$(curl -s -X POST -H "Content-Type: application/json" https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/tokens -d '{"auth:{"apiAccessKeyCredentials":{"accessKey":"'$HP_ACCESS_KEY'", "secretKey":"'$HP_SECRET_KEY'"}, "tenantId":"'$HP_PROJECT_ID'"}}' | grep "HPAuth" | sed 's/^    \"id\": \"//' | sed 's/\",//')
- 
-curl -i -H "x-auth-token: $HPAuth" https://region-a.geo-1.objects.hpcloudsvc.com/v1/$HP_PROJECT_ID/$container/$test_file_name -X PUT -T $test_file
 
+#
+###
+
+export test_file_name=$(basename $test_file)
+export HPAuth=$(curl -s -X POST -H "Content-Type: application/json" https://region-a.geo-1.identity.hpcloudsvc.com:35357/v2.0/tokens -d '{"auth:{"apiAccessKeyCredentials":{"accessKey":"'$HP_ACCESS_KEY'", "secretKey":"'$HP_SECRET_KEY'"}, "tenantId":"'$HP_PROJECT_ID'"}}' | grep "HPAuth" | sed 's/^    \"id\": \"//' | sed 's/\",//') 
+curl -i -H "x-auth-token: $HPAuth" https://region-a.geo-1.objects.hpcloudsvc.com/v1/$HP_PROJECT_ID/$container/$test_file_name -X PUT -T $test_file
 curl -i -H "x-auth-token: $HPAuth" https://region-b.geo-1.objects.hpcloudsvc.com/v1/$HP_PROJECT_ID/$container/$test_file_name -X PUT -T $test_file
