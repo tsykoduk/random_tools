af8593c8113c8ca601928171cf206e573d787fdb
Greg Nokes <tsykoduk@gmail.com>
2013-10-22 09:10:33 -0700

    added some error and env checking to swift backup

--- swift_backup.sh
+++ swift_backup.sh
@@ -2,22 +2,56 @@
 
 #####
 # You will need to install the swift python client to get this to work
-# check out https://community.hpcloud.com/article/python-swiftclient-linux-installation 
+# check out https://community.hpcloud.com/article/python-swiftclient-linux-installation
 # for directions
+#
+# You will want to put this into a directory called backups in the home directory of the user
+# who will run this
 #####
+
+#####
+# Config below
+# The name must be unique across all servers using the same swift container.
+
+Swift_Container="Server_Backups"
+Server_Unique_Name="Server"
+Backups_Home_Directory="/home/ubuntu/backups"
+
+echo $Swift_Container
+echo $Server_Unique_Name
+echo $Backups_Home_Directory
+#
+####
+
+# Env Checks
+
+echo "checking for my directories"
+
+if [ ! -d $Backups_Home_Directory ]; then
+  echo "woops, something is wrong. My expected home directory is missing. Perhaps a typo in the config?"
+  exit 1
+fi
+
+if [ ! -d "${Backups_Home_Directory}/scratch" ]; then
+  echo "woops, my scratch directory is missing. I'll create a new one"
+  mkdir $Backups_Home_Directory/scratch
+fi
+
+if [ ! -d "${Backups_Home_Directory}/backup_storage" ]; then
+  echo "woops, my storage directory is missing. I'll create a new one"
+  mkdir $Backups_Home_Directory/backup_storage
+fi
 
 
 #start process, create a unique filename
 now=`date "+%Y-%m-%d-%H-%M-%S"`
-target="backups_${now}.tgz"
+target="${Server_Unique_Name}_${now}.tgz"
 
 echo "starting backup"
 echo "moving backup assets over"
 
 #clean up after last run, just incase something went south.
 rm -rf ~/backups/scratch/*
-
-
 
 # Anything you put into the scratch directory will be backed up, so you can use things like
 # MySQL dump or what have you, and just put the resultant files in ~/backups/scratch
@@ -40,9 +74,7 @@ tar -czf $target scratch/*
 
 #Upload to Object Store. You'll want to insure that the container is set correctly
 
-Swift_Container="Server_Backups"
-
-echo "uploading to object storage" 
+echo "uploading to object storage"
 swift upload $Swift_Container $target
 
 #Clean up
