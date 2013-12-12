6acab870fe020498e300c83e9dc1c826149e482a
Greg Nokes <tsykoduk@gmail.com>
2013-12-12 09:43:24 -0800

    updated giflister builder to see jogs and pngs

--- siteupdater.sh
+++ siteupdater.sh
@@ -6,12 +6,12 @@ unset GIT_DIR
 echo "Starting to push to production"
 cd /home/ubuntu/www
 
-echo "Get updates"
+# Get updates
 git pull -v
 cd output
 
-echo "generate a new gitlister.html file"
-files=`find *.gif`
+#generate a new gitlister.html file
+files=`find .  -name "*.gif" -o -name "*.jpg" -o -name "*.png"`
 rm ./giflister.html
 touch ./giflister.html
 output=./giflister.html
@@ -20,7 +20,8 @@ for f in $files; do (
 )
 done
 
-echo "push to prod"
+#copy stuff over, and set the permissions correctly
+
 sudo cp -R * /srv/www
 cd /srv/www
 sudo chown -R www-data:www-data *
